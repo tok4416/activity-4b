764a02e11321559d68f0bfd7b0274b7090f1e329
Miles Tallia (RIT Student) <mot7506@rit.edu>
2024-11-19 13:56:17 -0500

    Test GPA function!

--- src/test/java/edu/rit/swen253/page/GPA/GPAPage.java
+++ src/test/java/edu/rit/swen253/page/GPA/GPAPage.java
@@ -2,6 +2,9 @@ package edu.rit.swen253.page.GPA;
 
 import edu.rit.swen253.page.AbstractAngularPage;
 import edu.rit.swen253.utils.DomElement;
+
+import java.util.List;
+
 import org.openqa.selenium.By;
 
 public class GPAPage extends AbstractAngularPage {
@@ -22,12 +25,42 @@ public class GPAPage extends AbstractAngularPage {
   }
   
   public void clickCalculate() {
-    DomElement surrounder = angularView.findChildrenBy(By.className("primaryButton")).get(0);
-    DomElement button = surrounder.findChildBy(By.cssSelector("button"));
+    DomElement button = angularView.findChildrenBy(By.className("primaryButton")).get(0);
     button.click();
   }
 
   public int getClassCount() {
     return angularView.findChildrenBy(By.className("w3-animate-opacity")).size();
   }
+
+  public void addClassInfo(int index, int grade) {
+    List<DomElement> classes = angularView.findChildrenBy(By.className("w3-animate-opacity"));
+    DomElement classBox = classes.get(index);
+
+    List<DomElement> inputs = classBox.findChildrenBy(By.cssSelector("input"));
+    DomElement classname = inputs.get(0);
+
+    String classString = "Class: " + index;
+    classname.enterText(classString);
+
+    List<DomElement> dropdowns = classBox.findChildrenBy(By.cssSelector("select"));
+    DomElement gradeSelector = dropdowns.get(0);
+    List<DomElement> options = gradeSelector.findChildrenBy(By.cssSelector("option"));
+    DomElement selectedGrade = options.get(grade);
+    
+    selectedGrade.click();
+  }
+
+  public float getTermGPA(){
+    List<DomElement> GPAs = angularView.findChildrenBy(By.className("results"));
+    String gpa = GPAs.get(0).getText();
+    return Float.parseFloat(gpa);
+  }
+
+  public float getCumulativeGPA(){
+    List<DomElement> GPAs = angularView.findChildrenBy(By.className("results"));
+    String gpa = GPAs.get(1).getText();
+    return Float.parseFloat(gpa);
+  }
+
 }
