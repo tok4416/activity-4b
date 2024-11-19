4cc1edc3566996de199a6cda84ff9ebaf0acbc53
Miles Tallia (RIT Student) <mot7506@rit.edu>
2024-11-19 13:16:16 -0500

    ADD and ReSET button usage tests

---
+++ src/test/java/edu/rit/swen253/test/GPA/ResetClassesGPATest.java
@@ -0,0 +1,59 @@
+package edu.rit.swen253.test.GPA;
+
+import edu.rit.swen253.page.GPA.GPAPage;
+import edu.rit.swen253.test.AbstractWebTest;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class ResetClassesGPATest extends AbstractWebTest {
+
+  private GPAPage GPAPage;
+
+  //
+  // Test sequence
+  //
+
+  @Test
+  @Order(1)
+  @DisplayName("First, navigate to the page.")
+  void navigateToHomePage() {
+    GPAPage = navigateToPage("https://tigercenter.rit.edu/tigerCenterApp/api/gpa-calc", GPAPage::new);
+    assertNotNull(GPAPage);
+  }
+
+  @Test
+  @Order(2)
+  @DisplayName("Second, click on the GPA Calculator button and validate navigation.")
+  void addClasses() {
+    GPAPage.clickAdd();
+    GPAPage.clickAdd();
+    GPAPage.clickAdd();
+    GPAPage.clickAdd();
+    GPAPage.clickAdd();
+  }
+
+  @Test
+  @Order(3)
+  @DisplayName("Third, make sure the classes have been added.")
+  void checkCount() {
+    assertEquals(6, GPAPage.getClassCount());
+  }
+
+  @Test
+  @Order(4)
+  @DisplayName("Fourth, reset all classes.")
+  void resetClasses() {
+    GPAPage.clickReset();
+  }
+
+  @Test
+  @Order(5)
+  @DisplayName("Lastly, re-check the count.")
+  void checkCountAfterReset() {
+    assertEquals(1, GPAPage.getClassCount());
+  }
+
+}
