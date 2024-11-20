a824519d28f4d2f4c505b33ae61e274a76c9f1c8
Duncan French <duncanlfrench@gmail.com>
2024-11-20 13:25:38 -0500

    Added tests for the case of searching for only graduate courses. Removed print statement from the ChooseUndergraduateCourseTest file.

--- src/test/java/edu/rit/swen253/test/search/ChooseGraduateCourseTest.java
+++ src/test/java/edu/rit/swen253/test/search/ChooseGraduateCourseTest.java
@@ -3,15 +3,15 @@ package edu.rit.swen253.test.search;
 import edu.rit.swen253.page.search.ClassSearchPage;
 import edu.rit.swen253.page.tiger.TigerCenterHomePage;
 import edu.rit.swen253.test.AbstractWebTest;
-
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
+
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class ChooseUndergraduateCourseTest extends AbstractWebTest {
+class ChooseGraduateCourseTest extends AbstractWebTest {
 
   private TigerCenterHomePage homePage;
   private ClassSearchPage searchPage;
@@ -43,7 +43,7 @@ class ChooseUndergraduateCourseTest extends AbstractWebTest {
   @Test
   @Order(4)
   public void selectCourseLevel() {
-    searchPage.selectAdvancedSearchLevel("Undergraduate");
+    searchPage.selectAdvancedSearchLevel("Graduate");
     searchPage.closeAdvancedSearchDialog();
   }
 
@@ -59,7 +59,7 @@ class ChooseUndergraduateCourseTest extends AbstractWebTest {
   public void verifyCourseLevels() {
     List<Integer> courseLevels = searchPage.findResultCourseLevels();
     for (Integer courseLevel : courseLevels) {
-      assertTrue(courseLevel < 600); // undergraduate courses have a level # below 600
+      assertTrue(courseLevel >= 600); // graduate courses have a level # of 600 or higher
     }
-  }
+  } 
 }
